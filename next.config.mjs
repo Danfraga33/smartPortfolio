00366375a58a45710ff4f3909559ac86d2df6141
Daniel Fraga <dan.fraga33@gmail.com>
2024-06-10 09:09:30 +0100

    perf: include relevant image urls in next config:

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["api.microlink.io", "images.unsplash.com"],
+  },
+};
 
 export default nextConfig;
