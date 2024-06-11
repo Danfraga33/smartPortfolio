7bddc655aa56f75158ed83b6e3c1b41f11d7c726
Daniel Fraga <dan.fraga33@gmail.com>
2024-06-11 19:48:18 +0100

    fix: footer cleanup

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer>
-      <nav className="mx-auto flex max-w-3xl gap-3 p-3">
+      <nav className="mx-auto flex justify-center hover:text-violet-200 transition-all max-w-6xl p-3">
         <Link href="/privacy">Privacy</Link>
       </nav>
     </footer>
