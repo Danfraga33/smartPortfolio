e3897f2fe548a72129e05c5eead2779524f07e6f
Daniel Fraga <dan.fraga33@gmail.com>
2024-07-29 18:58:00 +0100

    fix: change text on home page

--- src/app/components/TextGenerateEffect.tsx
+++ src/app/components/TextGenerateEffect.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { TextGenerateEffect } from "./ui/text-generate-effect";
 
-const words = `Many developers can solve technical challenges, but understanding the business problem is key. A developer who aligns technical solutions with business needs is indispensable, driving growth and profitability. This ability to translate business requirements into impactful solutions makes them a valuable asset to any company.`;
+const words = `Translating business needs into impactful technical solutions that drive growth and profitability.`;
 
 export function TextGenerateEffectComponent() {
   return <TextGenerateEffect words={words} />;
