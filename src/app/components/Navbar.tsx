81b3b7d3dff018f9393131d8b1aef54302ef25f0
Daniel Fraga <dan.fraga33@gmail.com>
2024-10-01 11:37:38 +0200

    feat: add blog link

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,6 +8,7 @@ import { usePathname } from "next/navigation";
 const Navbar = () => {
   const router = usePathname();
   const isWorks = router === "/works";
+  const isBlog = router === "/blog";
   return (
     <header className="top-0 bg-transparent">
       <div className="max-w-6xl mx-auto flex flex-wrap justify-between gap-3 px-3 py-4">
@@ -24,12 +25,26 @@ const Navbar = () => {
           >
             works
           </Link>
+          <a
+            href="https://stoic-dev-blogger.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={` active:text-[#383595]  dark:active:text-[#da9106] hover:text-gray-500/65 ${isBlog ? "text-[#383595] font-semibold dark:font-semibold dark:text-[#fd9106]" : ""}`}
+          >
+            blog
+          </a>
         </nav>
         <div className="flex items-center gap-4">
           <AiChatButton />
           <ThemeToggle />
           <Button className="bg-[#ffef] text-black  dark:bg-[#222] dark:text-white font-medium p-2">
-            <a href="https://danielfraga.dev/#contact">Contact</a>
+            <a
+              href="https://danielfraga.dev/#contact"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Contact
+            </a>
           </Button>
         </div>
       </div>
