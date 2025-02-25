93a04e41d509df44a72c95b89c552f77c4899a81
Daniel Fraga <dan.fraga33@gmail.com>
2025-02-26 09:41:46 +1100

    fix: change Link to React Scroll Link

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,6 +4,7 @@ import ThemeToggle from "./ThemeToggle";
 import AiChatButton from "./AiChatButton";
 import { Button } from "./ui/moving-border";
 import { usePathname } from "next/navigation";
+import { Link as ScrollLink } from "react-scroll";
 
 const Navbar = () => {
   const router = usePathname();
@@ -13,18 +14,22 @@ const Navbar = () => {
     <header className="top-0 bg-transparent">
       <div className="max-w-6xl mx-auto flex flex-wrap justify-between gap-3 px-3 py-4">
         <nav className="space-x-4 font-medium uppercase tracking-tighter ">
-          <Link
-            href="/"
+          <ScrollLink
+            to="/"
+            smooth={true}
+            duration={700}
             className={`active:text-[#383595] dark:hover:text-gray-300 hover:text-gray-500/65 transition-all dark:active:text-[#da9106]]  ${!isWorks ? "font-semibold text-[#383595] dark:text-[#fd9106]" : ""}`}
           >
             home
-          </Link>
-          <Link
-            href="/works"
-            className={` active:text-[#383595]  dark:active:text-[#da9106] hover:text-gray-500/65 ${isWorks ? "text-[#383595] font-semibold dark:font-semibold dark:text-[#fd9106]" : ""}`}
+          </ScrollLink>
+          <ScrollLink
+            to="works"
+            smooth={true}
+            duration={700}
+            className={`cursor-pointer active:text-[#383595]  dark:active:text-[#da9106] hover:text-gray-500/65 ${isWorks ? "text-[#383595] font-semibold dark:font-semibold dark:text-[#fd9106]" : ""}`}
           >
             works
-          </Link>
+          </ScrollLink>
           <a
             href="https://ai-blog-snowy-glitter-4565.fly.dev/blogs"
             target="_blank"
@@ -38,13 +43,9 @@ const Navbar = () => {
           <AiChatButton />
           <ThemeToggle />
           <Button className="bg-[#ffef] text-black  dark:bg-[#222] dark:text-white font-medium p-2">
-            <Link
-              href="https://danielfraga.dev/#contact"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <ScrollLink to="contact" smooth={true} duration={700}>
               Contact
-            </Link>
+            </ScrollLink>
           </Button>
         </div>
       </div>
